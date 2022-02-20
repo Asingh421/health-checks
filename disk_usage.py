3068c6d547e5e7a9d9c480720ae0f82e9f049540
Abhay Singh <48217430+Asingh421@users.noreply.github.com>
2022-02-21 00:07:32 +0530

    Update disk_usage.py

--- disk_usage.py
+++ disk_usage.py
@@ -1,1 +1,17 @@
-#ile to trace disk usage
+#file to trace disk usage
+# Python program to explain shutil.disk_usage() method
+	
+# importing shutil module
+import shutil
+
+# Path
+path = "/home/User/Documents"
+
+# Get the disk usage statistics
+# about the given path
+stat = shutil.disk_usage(path)
+
+# Print disk usage statistics
+print("Disk usage statistics:")
+print(stat)
+
